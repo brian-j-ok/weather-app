312a57c74bc64b6fbf357c448edec7a0c059923a
brianjrok <brianjrok@gmail.com>
2022-02-21 21:19:14 -0500

    set up background img and webpack img asset managment

--- src/index.js
+++ src/index.js
@@ -15,6 +15,6 @@ if('geolocation' in navigator) {
 
 const getWeather = async () => {
   const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=e33abff516f8fc21eb34082ce41dc0bb', {mode: 'cors'});
-  const weatherData = await response.json();
-  console.log(weatherData);
+  const currentWeatherData = await response.json();
+  console.log(currentWeatherData);
 }
