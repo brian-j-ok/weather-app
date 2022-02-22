0539297cc511da7d384b2219b19f96c2b45a43ee
brianjrok <brianjrok@gmail.com>
2022-02-21 21:07:52 -0500

    set up geolocation for initial position and refactored fetch request

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import './styles/style.css';
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=e33abff516f8fc21eb34082ce41dc0bb', {mode: 'cors'})
-.then(function(response) {
-  return response.json();
-})
-.then(function(response) {
-  console.log(response);
-});
+let latitude = 0;
+let longitude = 0;
+
+if('geolocation' in navigator) {
+  navigator.geolocation.getCurrentPosition((position) => {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    getWeather();
+  })
+} else {
+  console.log('no geolocation');
+}
+
+const getWeather = async () => {
+  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=e33abff516f8fc21eb34082ce41dc0bb', {mode: 'cors'});
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
