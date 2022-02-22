7386d81df5c7c5fa1d85fb159ecc93c63a20c567
brianjrok <brianjrok@gmail.com>
2022-02-21 21:30:34 -0500

    switch API fetch to onecall over current

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ if('geolocation' in navigator) {
 }
 
 const getWeather = async () => {
-  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=e33abff516f8fc21eb34082ce41dc0bb', {mode: 'cors'});
+  const response = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&appid=e33abff516f8fc21eb34082ce41dc0bb', {mode: 'cors'});
   const currentWeatherData = await response.json();
   console.log(currentWeatherData);
 }
