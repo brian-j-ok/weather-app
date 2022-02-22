6b25116dceb1a94c7b18c0348f7d81f307b25b8a
brianjrok <brianjrok@gmail.com>
2022-02-21 23:53:21 -0500

    added forecast css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './styles/style.css';
 import './styles/forecast.css';
 
+import { format, fromUnixTime } from 'date-fns';
+
 let latitude = 0;
 let longitude = 0;
 
@@ -22,17 +24,39 @@ const getWeather = async () => {
 }
 
 function generateForecast(dailyWeatherData) {
-  console.log(dailyWeatherData);
   const forecast = document.getElementById('forecast');
 
   dailyWeatherData.forEach((day) => {
+    const dateTime = fromUnixTime(day.dt);
+
+    // Creates Daily Forecast Container
     let dailyForecast = document.createElement('div');
     dailyForecast.classList.add('daily-forecast');
 
+    // Finds the day of the week for forecast and displays as a header
+    let weekDay = document.createElement('h2');
+    weekDay.innerText = format(dateTime, 'EEE');
+
+    // Creates Forecast Image and Sets Data Attribute which links to appropriate image in forecast.css
     let dailyForecastImage = document.createElement('img');
     dailyForecastImage.setAttribute('data-weather', day.weather[0].main);
 
+    // Create Min and Max Temperature
+    let dailyForecastMinMax = document.createElement('div');
+    dailyForecastMinMax.classList.add('min-max');
+
+    let dailyForecastMax = document.createElement('p');
+    dailyForecastMax.innerText = Math.round((day.temp.max - 273.15) * 9/5 + 32);
+    let dailyForecastMin = document.createElement('p');
+    dailyForecastMin.innerText = Math.round((day.temp.min - 273.15) * 9/5 + 32);
+
+    dailyForecastMinMax.appendChild(dailyForecastMax);
+    dailyForecastMinMax.appendChild(dailyForecastMin);
+
+    // Append all elements together
+    dailyForecast.appendChild(weekDay);
     dailyForecast.appendChild(dailyForecastImage);
+    dailyForecast.appendChild(dailyForecastMinMax);
 
     forecast.appendChild(dailyForecast);
   });
