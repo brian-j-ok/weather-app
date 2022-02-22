5d062cf861a7032f07aa27d3d7ef5b075fc94264
brianjrok <brianjrok@gmail.com>
2022-02-21 20:47:35 -0500

    set up fetch request to retrieve data from OpenWeather API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles/style.css';
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=e33abff516f8fc21eb34082ce41dc0bb', {mode: 'cors'})
+.then(function(response) {
+  return response.json();
+})
+.then(function(response) {
+  console.log(response);
+});
