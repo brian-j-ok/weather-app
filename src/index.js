f7091a506fc1fff1705b053e90ae9fe3070fb4b2
brianjrok <brianjrok@gmail.com>
2022-02-22 17:06:17 -0500

    added search functionality and minor additions to UI

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import './styles/style.css';
 import './styles/forecast.css';
+import './styles/current.css';
 
 import { format, fromUnixTime } from 'date-fns';
 
 let latitude = 0;
 let longitude = 0;
+
+let city = null;
+let state = null;
+
+const searchBar = document.getElementById('search');
+const searchBtn = document.getElementById('search-btn');
+searchBtn.addEventListener('click', () => {
+  search();
+})
 
 if('geolocation' in navigator) {
   navigator.geolocation.getCurrentPosition((position) => {
@@ -17,14 +27,85 @@ if('geolocation' in navigator) {
 }
 
 const getWeather = async () => {
-  const response = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=minutely,hourly&appid=e33abff516f8fc21eb34082ce41dc0bb', {mode: 'cors'});
-  const weatherData = await response.json();
-  console.log(weatherData);
-  generateForecast(weatherData.daily);
+  try {
+    const response = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=minutely,hourly&appid=e33abff516f8fc21eb34082ce41dc0bb', {mode: 'cors'});
+    const weatherData = await response.json();
+    console.log(weatherData);
+    await reverseGeocode();
+    generateCurrent(weatherData.current);
+    generateForecast(weatherData.daily);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
+const reverseGeocode = async () => {
+  try {
+    const response = await fetch('http://api.openweathermap.org/geo/1.0/reverse?lat=' + latitude + '&lon=' + longitude + '&limit=1&appid=e33abff516f8fc21eb34082ce41dc0bb', {mode: 'cors'});
+    const locationData = await response.json();
+    city = locationData[0].name;
+    state = locationData[0].state;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Search Bar Functionality
+async function search() {
+  let input = searchBar.value.split(',');
+
+  let locationData = null;
+
+  if (input.size > 1) {
+    city = input[0].replace(/\s+/g, '');
+    state = input[1].replace(/\s+/g, '');
+
+    const response = await fetch ('http://api.openweathermap.org/geo/1.0/direct?q=' + city + ',' + state + ',US&limit=1&appid=e33abff516f8fc21eb34082ce41dc0bb');
+    locationData = await response.json();
+  } else {
+    city = input[0];
+
+    const response = await fetch ('http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=e33abff516f8fc21eb34082ce41dc0bb');
+    locationData = await response.json();
+  }
+
+  latitude = locationData[0].lat;
+  longitude = locationData[0].lon;
+
+  getWeather();
+}
+
+// Create Current Weather Forecast with DOM Elements
+function generateCurrent(currentWeatherData) {
+  const current = document.getElementById('current');
+
+  while (current.firstChild) {
+    current.removeChild(current.firstChild);
+  }
+
+  const dateTime = fromUnixTime(currentWeatherData.dt);
+
+  let location = document.createElement('h2');
+  location.innerText = city + ', ' + state;
+
+  let currentWeatherImage = document.createElement('img');
+  currentWeatherImage.setAttribute('data-weather', currentWeatherData.weather[0].main);
+
+  let currentWeather = document.createElement('h3');
+  currentWeather.innerText = currentWeatherData.weather[0].main;
+
+  current.appendChild(location);
+  current.appendChild(currentWeatherImage);
+  current.appendChild(currentWeather);
+}
+
+// Create 7 Day Forecast with DOM Elements
 function generateForecast(dailyWeatherData) {
   const forecast = document.getElementById('forecast');
+
+  while (forecast.firstChild) {
+    forecast.removeChild(forecast.firstChild);
+  }
 
   dailyWeatherData.forEach((day) => {
     const dateTime = fromUnixTime(day.dt);
